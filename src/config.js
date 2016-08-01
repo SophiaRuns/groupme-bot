e794a12c2421c1c9f143a4b0e74ce34f957b897d
SophiaRuns <sgfarquh@mtu.edu>
2016-08-01 16:01:54 -0500

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,18 @@
+var config = {};
+
+config.bots = [
+  {
+    botID: '	835e283821a95ea7e88da684e2',
+    groupID: 'xxxxxxxx',
+    groupLocalID: '1', // an identifier for this group used in this application only
+    botName: 'mrroboto',
+    groupName: 'the-group-name' // optional
+  }
+];
+
+config.yahooWeatherAppId = '';
+
+// Travis CI user token
+config.travisUserToken = 'xxxxxxxxxxxxxx';
+
+module.exports = config;
